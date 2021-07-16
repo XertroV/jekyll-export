b501a53038bafe3f591fcf36d0431dc7201168bb
Max Kaye <m@xk.io>
2021-07-16 10:05:30 +1000

    give users options

--- lib/jekyll/export_filters.rb
+++ lib/jekyll/export_filters.rb
@@ -10,7 +10,23 @@ module Jekyll
     def contains_html(body)
       body.match?(%r!<[^ >]+( .*)?/?>!)
     end
+
+    def date_to_ts(date)
+      date.to_i
+    end
+  end
+
+  class CurrentTimeTag < Liquid::Tag
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text
+    end
+
+    def render(_context)
+      "#{Time.now.to_i}"
+    end
   end
 end
 
+Liquid::Template.register_tag("current_date", Jekyll::CurrentTimeTag)
 Liquid::Template.register_filter(Jekyll::ExportFilters)
